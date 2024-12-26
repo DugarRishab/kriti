70fec261ed53411495c033e75cc5c132fb503bd3
iamakashtechie <akash.techie01@gmail.com>
2024-12-26 17:39:11 +0530

    [UPDATE]: fixed the responsiveness of BannerSection

--- src/components/BannerSection/BannerSection.jsx
+++ src/components/BannerSection/BannerSection.jsx
@@ -7,15 +7,14 @@ const BannerSection = () => {
 		<section className="section2">
 			<div className="section2__main">
 				<div className="main__leftchild">
-					<div className="ruslan heading leftchild__heading">
-						VIVID. <br />
-						VIBRANT. <br />
-						VISIONARY.
-					</div>
-					<div className="description leftchild__description">
-						Indulge into the experience of boundless creativity and
-						artistic expression.
-					</div>
+						<div className="ruslan heading leftchild__heading">
+							VIVID. <br />
+							VIBRANT. <br />
+							VISIONARY.
+						</div>
+						<div className="description leftchild__description">
+							Indulge into the experience of boundless creativity and artistic expression.
+						</div>
 				</div>
 				<div className="right__child">
 					<img src="./imgs/home/floral-1.png" alt="" />
