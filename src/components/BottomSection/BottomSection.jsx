d578d999058d63983d50bcb9d62de290dd8cc065
Dream-World-Coder <dreamy.star.368@gmail.com>
2025-01-07 19:33:50 +0530

    updated phone number

--- src/components/BottomSection/BottomSection.jsx
+++ src/components/BottomSection/BottomSection.jsx
@@ -2,15 +2,15 @@ import React, { useState, useEffect } from "react";
 import "./BottomSection.css";
 
 const BottomSection = () => {
-    return (
-        <section className="bottom-section">
-            <h1>SEE YOU THERE</h1>
-            <p>REACH OUT TO MAYUKH @1234567890 IF YOU HAVE ANY QUESTIONS</p>
-            <img className="team-image" src="./imgs/home/team.png" alt="" />
-            <p className="team-scage">TEAM SCAGE</p>
-            <img className="sunshine" src="./imgs/home/sunshine.png" alt="" />
-        </section>
-    );
+  return (
+    <section className="bottom-section">
+      <h1>SEE YOU THERE</h1>
+      <p>REACH OUT TO MAYUKH @9749165361 IF YOU HAVE ANY QUESTIONS</p>
+      <img className="team-image" src="./imgs/home/team.png" alt="" />
+      <p className="team-scage">TEAM SCAGE</p>
+      <img className="sunshine" src="./imgs/home/sunshine.png" alt="" />
+    </section>
+  );
 };
 
 export default BottomSection;
