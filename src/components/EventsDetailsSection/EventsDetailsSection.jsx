f1185849254c9dc2173d0b0cbb686337f590e4ee
Dream-World-Coder <dreamy.star.368@gmail.com>
2025-01-17 14:02:08 +0530

    [UPDATE]: updated button text for last 3 events, No registration required

--- src/components/EventsDetailsSection/EventsDetailsSection.jsx
+++ src/components/EventsDetailsSection/EventsDetailsSection.jsx
@@ -2,12 +2,23 @@ import React from "react";
 import Button from "../Button/Button";
 import "./EventsDetailsSection.css";
 
-const EventsDetailsSection = ({ isReversed, eventImage, eventName, eventDescription, eventLink }) => {
+const EventsDetailsSection = ({
+    isReversed,
+    eventImage,
+    eventName,
+    eventDescription,
+    eventLink,
+    isLinkVoid,
+}) => {
     return (
         <section className="EventsDetails-section">
-            <div className={`eventDetails ${isReversed ? 'reverse' : ''}`}>
+            <div className={`eventDetails ${isReversed ? "reverse" : ""}`}>
                 <div className="image">
-                    <img className="event-image" src={eventImage} alt={eventName} />
+                    <img
+                        className="event-image"
+                        src={eventImage}
+                        alt={eventName}
+                    />
                     {/* <img className="top-ribbon-image" src={`${isReversed ? './imgs/Green-Ribbon.png' : './imgs/Red-Ribbon.png'} `} alt="" /> */}
                     {/* <img className="bottom-ribbon-image" src={`${isReversed ? './imgs/Green-Ribbon.png' : './imgs/Red-Ribbon.png'} `} alt="" /> */}
                 </div>
@@ -15,23 +26,27 @@ const EventsDetailsSection = ({ isReversed, eventImage, eventName, eventDescript
                     <h1 className="ruslan">{eventName}</h1>
                     <p>{eventDescription}</p>
                     <a style={{ textDecoration: "none" }} href={eventLink}>
-                        <Button 
-                            text={"REGISTER"}
-					        endIcon={
-					        	<svg
-					        		width="28"
-					        		height="26"
-					        		viewBox="0 0 28 26"
-					        		fill="none"
-					        		xmlns="http://www.w3.org/2000/svg"
-					        	>
-					        		<path
-					        			d="M20.9527 11.3332L12.0127 2.39315L14.3697 0.0361328L27.3333 12.9998L14.3697 25.9633L12.0127 23.6063L20.9527 14.6665H0.666656V11.3332H20.9527Z"
-					        			fill="black"
-					        		/>
-					        	</svg>
-					        }>
-                        </Button>
+                        <Button
+                            text={
+                                isLinkVoid
+                                    ? "NO REGISTRATION REQUIRED"
+                                    : "REGISTER"
+                            }
+                            endIcon={
+                                <svg
+                                    width="28"
+                                    height="26"
+                                    viewBox="0 0 28 26"
+                                    fill="none"
+                                    xmlns="http://www.w3.org/2000/svg"
+                                >
+                                    <path
+                                        d="M20.9527 11.3332L12.0127 2.39315L14.3697 0.0361328L27.3333 12.9998L14.3697 25.9633L12.0127 23.6063L20.9527 14.6665H0.666656V11.3332H20.9527Z"
+                                        fill="black"
+                                    />
+                                </svg>
+                            }
+                        ></Button>
                     </a>
                 </div>
             </div>
--- src/screens/Events.jsx
+++ src/screens/Events.jsx
@@ -5,15 +5,15 @@ import EventsHeroSection from "../components/EventsHeroSection/EventsHeroSection
 import EventsDetailsSection from "../components/EventsDetailsSection/EventsDetailsSection";
 import Footer from "../components/Footer/Footer";
 import "./Events.css";
-import eventsData from '../eventsData.json';
+import eventsData from "../eventsData.json";
 
 const Events = () => {
     return (
         <div>
             {/* <Header /> */}
             <EventsHeroSection />
-            
-            {eventsData.events.map((event,index) => (
+
+            {eventsData.events.map((event, index) => (
                 <EventsDetailsSection
                     key={index}
                     isReversed={event.isReversed}
@@ -21,6 +21,7 @@ const Events = () => {
                     eventName={event.eventName}
                     eventDescription={event.eventDescription}
                     eventLink={event.eventLink}
+                    isLinkVoid={event.eventLink == "#" ? true : false}
                 />
             ))}
 
